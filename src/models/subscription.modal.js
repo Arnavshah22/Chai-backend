42dbea7026b89aa95eaf1690625c3537a7f4bc42
Arnav shah <arnavshaw2015@gmail.com>
2024-06-28 23:54:26 +0530

    Add comment,like,tweet and playlist models

--- src/models/subscription.modal.js
+++ src/models/subscription.modal.js
@@ -1,19 +1,16 @@
-import mongoose,{Schema} from "mongoose";
+import mongoose, {Schema} from "mongoose"
 
-const SubscriptionSchema=new Schema({
-    subscriber:{
-        type:Schema.Types.ObjectId,  // the one who is subscribing
-        ref:"User"
+const subscriptionSchema = new Schema({
+    subscriber: {
+        type: Schema.Types.ObjectId, // one who is subscribing
+        ref: "User"
     },
-    channel:{
-        type:Schema.Types.ObjectId,
-        //one to whom 'Subscriber' is subscribing
-        ref:"User",
+    channel: {
+        type: Schema.Types.ObjectId, // one to whom 'subscriber' is subscribing
+        ref: "User"
     }
-},{
-    timestamps:true,
+}, {timestamps: true})
 
-})
 
-export const Subscriptions=mongoose.model("Subscriptions",SubscriptionSchema);
 
+export const Subscription = mongoose.model("Subscription", subscriptionSchema)
